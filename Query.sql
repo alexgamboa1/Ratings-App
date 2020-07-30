50f3f3fbc201b0c52d11e5648be2aa2fffcf11cd
alexgamboa1 <ag.gamboa94@gmail.com>
2020-07-29 18:17:09 -0700

    commit rating based on reviewer

--- Query.sql
+++ Query.sql
@@ -31,6 +31,24 @@ FROM series JOIN reviews ON series.id = reviews.series_id
 GROUP BY genre ORDER BY avg_rating;
 
 
+-- Return the name of the reviewer, the amount of reviews given by the reviewer, min, max, and avg rating, and status. 
+-- The active status is based off if they have left a review or not use Active or NON ACTIVE. 
+
+SELECT first_name, last_name, 
+		COUNT(rating) AS COUNT, 
+        IFNULL(MIN(rating),0) AS MIN, 
+        IFNULL(MAX(rating),0) AS MAX, 
+        ROUND(IFNULL(AVG(rating),0)) AS AVG,
+        CASE 
+			WHEN COUNT(rating) >= 1 THEN 'ACTIVE'
+            ELSE 'NON ACTIVE'
+        END AS STATUS
+FROM reviewers LEFT JOIN reviews on reviewers.id = reviews.reviewer_id 
+GROUP BY reviewers.id;
+
+
+
+
 
 
 
