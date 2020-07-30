309b4536d4796b3e59b0b39f82015c8a7d55b0ab
alexgamboa1 <ag.gamboa94@gmail.com>
2020-07-29 17:53:53 -0700

    commit avg rating query

--- Query.sql
+++ Query.sql
@@ -8,3 +8,7 @@ ORDER BY title;
 
 -- Return the title and average ratingfor each show with lowest rating at the top. 
 
+SELECT title, AVG(rating) AS avg_rating
+FROM series JOIN reviews ON series.id = reviews.series_id
+GROUP BY series.id
+ORDER BY avg_rating;
